db7c47a5ef07d9cfea5b57af16a9b342f93ff8fa
Fibonacci <dave.green1664@yahoo.co.uk>
2021-10-08 00:49:38 +0100

    Add project files.

---
+++ SFML_Client/Main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+
+//void testApp()
+//{
+//    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+//    sf::CircleShape shape(100.f);
+//    shape.setFillColor(sf::Color::Green);
+//
+//    while (window.isOpen())
+//    {
+//        sf::Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//        }
+//
+//        window.clear();
+//        window.draw(shape);
+//        window.display();
+//    }
+//}
+
+int main()
+{
+	//testApp();
+
+	sf::TcpSocket tcpSocket;
+	sf::Time timeout = sf::seconds(10.0f);
+	
+	sf::Socket::Status socketStatus = tcpSocket.connect("127.0.0.1", 53000, timeout);
+
+	if (socketStatus != sf::Socket::Done)
+	{
+		std::cout << "Not connected!\n";
+	}
+
+	return 0;
+}
